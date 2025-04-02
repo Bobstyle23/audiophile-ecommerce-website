0e2f01a395a0a2627afd6adb55207fb9ba67b5f3
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-04-02 16:48:32 +0900

    Add product included list mobile version

--- src/js/category-detail.js
+++ src/js/category-detail.js
@@ -1,9 +1,25 @@
 const allDetailData = JSON.parse(sessionStorage.getItem("productData"));
 const searchParam = new URLSearchParams(window.location.search);
 const productSlug = searchParam.get("id");
+const productIncludeList = document.querySelector(".included__list");
 
-const productDetail = allDetailData.find(
-  (element) => element.slug === productSlug,
-);
+const {
+  includes,
+  gallery,
+  name,
+  others,
+  description,
+  features,
+  categoryImage,
+  price,
+} = allDetailData.find((element) => element.slug === productSlug);
 
-console.log(productDetail);
+for (let included of includes) {
+  let productIncludeItem = document.createElement("li");
+  let productIncludeItemQuantity = document.createElement("span");
+  productIncludeItem.classList.add("included__list--item");
+  productIncludeItemQuantity.textContent = `${included.quantity}x`;
+  productIncludeItem.textContent = `${included.item}`;
+  productIncludeItem.prepend(productIncludeItemQuantity);
+  productIncludeList.appendChild(productIncludeItem);
+}
