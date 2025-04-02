141c8e65ba3f41588e23e85fa28938442863d8dd
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-04-02 21:22:54 +0900

    Modify recommended section to change data dynamically

--- src/js/category-detail.js
+++ src/js/category-detail.js
@@ -3,6 +3,7 @@ const searchParam = new URLSearchParams(window.location.search);
 const productSlug = searchParam.get("id");
 const productIncludeList = document.querySelector(".included__list");
 const featuresContainer = document.querySelector(".features");
+const recommendations = document.querySelector(".recommendations");
 
 const {
   includes,
@@ -14,6 +15,33 @@ const {
   categoryImage,
   price,
 } = allDetailData.find((element) => element.slug === productSlug);
+
+// NOTE: recommendations
+for (let recommended of others) {
+  const recommendationContainer = document.createElement("article");
+  recommendationContainer.classList.add("recommendation");
+  recommendationContainer.innerHTML = `
+   <picture class="recommendation__image">
+      <source
+        srcset=${recommended.image.desktop}
+        media="(min-width: 75em)"
+        type="image/webp"
+      />
+      <source
+        srcset=${recommended.image.tablet}
+        media="(min-width: 48em)"
+        type="image/webp"
+      />
+      <img
+        src=${recommended.image.mobile}
+        alt=""
+      />
+    </picture>
+    <h3 class="recommendation__title">${recommended.name}</h3>
+    <a href="#" class="btn btn__primary recommendation__link">See product</a>
+`;
+  recommendations.appendChild(recommendationContainer);
+}
 
 // NOTE: features
 for (let featureText of features.split("\n\n")) {
