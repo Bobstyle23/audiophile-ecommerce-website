ec8165fe7a6c43f0d67a98dba87ea26302818121
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-05-09 17:02:31 +0900

    Populate cart items data into checkout page

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,18 @@
 const goBackBtn = document.querySelector(".checkout__back-btn");
+const cartItems = JSON.parse(localStorage.getItem("cart"));
+const itemsContainer = document.querySelector(".summary__items");
+import { formatPrice } from "./utilities.js";
+
+for (let item of cartItems) {
+  itemsContainer.innerHTML += `
+<div>
+ <img src=${item.itemImage} class="items__img" />
+ <h3 class="items__name">${item.itemName.split(" ")[0]}</h3>
+ <small class="items__price">${formatPrice(item.itemPrice)}</small>
+ <p class="items__count">x${item.itemCount}</p>
+</div>
+`;
+}
 
 goBackBtn.addEventListener("click", () => {
   history.back();
